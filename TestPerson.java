e93e6d3c1c49c9572987a971f26993ee6a103e5d
sameer <sameerktiwari@outlook.com>
2017-10-27 15:25:25 +0530

    For Jenkins

---
+++ TestPerson.java
@@ -0,0 +1,22 @@
+/**
+ * Hello world!
+ *
+ */
+public class TestPerson 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!"+greetMe());
+    }
+	
+	public static String greetMe(){
+		String name="Sameer";
+		return "Welcome: "+name;
+	}
+	
+	public static int add(int num1,int num2){
+		System.out.println("Addition of two numbers: "+(num1+num2));
+		return (num1+num2);
+	}
+	
+}
